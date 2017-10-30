55792f1dc220d191ef300f709d5b91e7074ddf35
aparna1998 <33229962+aparna1998@users.noreply.github.com>
2017-10-30 21:59:58 +0530

    Add files via upload
    insertion sort program added

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,33 +1,30 @@
-// Java program for implementation of Insertion Sort
-class InsertionSort{
-    void sort(int arr[]){
-        int n = arr.length;
-        for (int i=1; i<n; ++i){
-            int key = arr[i];
-            int j = i-1;
- 
-            while (j>=0 && arr[j] > key){
-                arr[j+1] = arr[j];
-                j = j-1;
-            }
-            arr[j+1] = key;
-        }
-    }
- 
-    static void printArray(int arr[]){
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i] + " ");
- 
-        System.out.println();
-    }
- 
-    public static void main(String args[]){        
-        int arr[] = {12, 11, 13, 5, 6};
- 
-        InsertionSort ob = new InsertionSort();        
-        ob.sort(arr);
-         
-        printArray(arr);
-    }
-}
+    public class insertionSort {  
+        public static void insertionSort(int array[]) {  
+            int n = array.length;  
+            for (int j = 1; j < n; j++) {  
+                int key = array[j];  
+                int i = j-1;  
+                while ( (i > -1) && ( array [i] > key ) ) {  
+                    array [i+1] = array [i];  
+                    i--;  
+                }  
+                array[i+1] = key;  
+            }  
+        }  
+           
+        public static void main(String a[]){    
+            int[] arr1 = {9,14,3,2,43,11,58,22};    
+            System.out.println("Before Insertion Sort");    
+            for(int i:arr1){    
+                System.out.print(i+" ");    
+            }    
+            System.out.println();    
+                
+            insertionSort(arr1);
+               
+            System.out.println("After Insertion Sort");    
+            for(int i:arr1){    
+                System.out.print(i+" ");    
+            }    
+        }    
+    }    
