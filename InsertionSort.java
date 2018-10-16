827afcfdb6ef220d92a67b289780c6f82db9c0b5
Vinamra Pankaj Sareen <vsareen24@gmail.com>
2018-10-16 22:42:05 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,25 +2,30 @@
 class InsertionSort 
 { 
     /*Function to sort array using insertion sort*/
-    void sort(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=1; i<n; ++i) 
-        { 
-            int key = arr[i]; 
-            int j = i-1; 
-  
-            /* Move elements of arr[0..i-1], that are 
-               greater than key, to one position ahead 
-               of their current position */
-            while (j>=0 && arr[j] > key) 
-            { 
-                arr[j+1] = arr[j]; 
-                j = j-1; 
-            } 
-            arr[j+1] = key; 
-        } 
-    } 
+    void sort ( int A[]) {
+     int n=A.length;
+        
+     for( int i = 0 ;i < n ; i++ ) {
+    /*storing current element whose left side is checked for its 
+             correct position .*/
+
+      int temp = A[ i ];    
+      int j = i;
+
+       /* check whether the adjacent element in left side is greater or
+            less than the current element. */
+
+          while(  j > 0  && temp < A[j-1]) {
+
+           // moving the left side element to one position forward.
+                A[j] = A[j-1];   
+                j= j - 1;
+
+           }
+         // moving current element to its  correct position.
+           A[ j ] = temp;       
+     }  
+}
   
     /* A utility function to print array of size n*/
     static void printArray(int arr[]) 
